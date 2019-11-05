449c8b5500663ab9a8920bdeb996ca6cac20c9ca
jcvasconcelos <jcvasconcelos@devants.com.br>
2019-11-05 20:42:30 -0300

    update 5-11

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -15,4 +15,19 @@ export class AboutusComponent implements OnInit {
   }
 }
 
+	mudatexto(i){
+	  	if(i==1){
+	  		document.getElementById("texto").innerHTML="Aqui vai o texto do Igor";
+	  	} else if(i==2){
+	  		document.getElementById("texto").innerHTML="E aqui o do Daniel";
+	  	} else if(i==3){
+	  		document.getElementById("texto").innerHTML="Esse é o do Lucas";
+	  	} else if(i==4){
+	  		document.getElementById("texto").innerHTML="O mais genial de todos... o do Cayo";
+	  	} else if(i==5){
+	  		document.getElementById("texto").innerHTML="E o patético de Julio";
+	  	} else{
+	  		document.getElementById("texto").innerHTML="Normal";
+	  	}
+	  }
 }
