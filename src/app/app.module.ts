402a40c9fb8efe328412c4e615c8a03156665322
jcvasconcelos <jcvasconcelos@devants.com.br>
2019-11-12 01:31:46 -0300

    refatorando codigo e deixando mais responsivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { SliderComponent } from './slider/slider.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { SimulationComponent } from './simulation/simulation.component';
 import { HomeComponent } from './home/home.component';
@@ -15,7 +13,6 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SliderComponent,
     AboutusComponent,
     SimulationComponent,
     HomeComponent
